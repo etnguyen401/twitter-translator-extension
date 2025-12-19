a52487cc42ed15b64b6d7a2af94265d20ab17df7
etnguyen401 <nguyeneric280@gmail.com>
2025-12-18 22:03:26 -0800

    initial logic for getting foreign language text from each tweet

--- scripts/content.js
+++ scripts/content.js
@@ -1,31 +1,63 @@
+let nativeLangs = ['en', 'und'];
 let targetNode = null;
+let observer = null;
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
     //if url changed, need to select new section
     if (msg.type === 'URL_CHANGE') {
         console.log("URL changed to: " + msg.url);
-
+        targetNode = null;
         //select until page finished loading, in increments
-        setInterval(() => {
+        //disconnect old observer
+        if (observer) {
+            observer.disconnect();
+            observer = null;
+        }
+
+        const intervalId = setInterval(() => {
+            console.log("Inside setInterval")
             if (!targetNode) {
-                targetNode = document.querySelector('section > div > div');
+                targetNode = document.querySelector('section > div > div[style^="position: relative"]');
                 if (targetNode) {
                     console.log("Target Node:", targetNode);
-                    clearInterval(this);
+                    //make options
+                    const options = {childList: true};
+                    //make callback for mutation observer
+                    const callback = function (mutationList, observer) {
+                        //for each mutation, log for now
+                        for (mutation of mutationList) {
+                            if (mutation.addedNodes.length > 0) {
+                                // console.log("New mutation: ", mutation);
+                                mutation.addedNodes.forEach((node) => {
+                                    //for each node, find descendent with lang attribute
+                                    let langDiv = node.querySelector('div[lang]');
+                                    //check if that div's lang is not in nativeLangs
+                                    if (langDiv && !nativeLangs.includes(langDiv.getAttribute('lang'))) {
+                                        console.log("Found foreign lang div:", langDiv); 
+                                    }
+                                });
+                            }
+                        }
+                    }
+                    //targetNode exists, so view existing nodes:
+                    //can probably refactor this into a function so above can use as well
+                    targetNode.querySelectorAll('div[lang]').forEach(node => {
+                        if (!nativeLangs.includes(node.getAttribute('lang'))) {
+                            console.log("Found foreign lang div (initial):", node);
+                        }
+                    });
+                    //create mutation observer, will take over and detect new nodes
+                    observer = new MutationObserver(callback);
+
+                    //start observing
+                    observer.observe(targetNode, options);
+                    
+                    clearInterval(intervalId);
+                    console.log("Interval cleared");
                 }
             }
-        }, 200);
+        }, 500);
+        
         
     }
 });
-
-const options = {childList: true};
-
-console.log("Content script loaded3");
-
-
-// const callback = function (mutationList, observer) {
-//     //for each mutation, 
-// }
-
-// const observer = new MutationObserver(callback);
-// observer.observe(targetNode, options);
+console.log("test5");
