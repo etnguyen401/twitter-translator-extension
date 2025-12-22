9c1da825b6affc24690366a190add9383dacadf6
etnguyen401 <nguyeneric280@gmail.com>
2025-12-22 01:17:29 -0800

    add logic for getting translation from self-hosted libretranslate

--- scripts/background.js
+++ scripts/background.js
@@ -5,10 +5,37 @@
 // console.log(text);
 console.log("Background script loaded");
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete') {
+    if (changeInfo.status === "complete") {
         chrome.tabs.sendMessage(tabId, {
             url: tab.url,
-            type: 'URL_CHANGE'
+            type: "URL_CHANGE"
         });
     }
+});
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.type !== "TRANSLATE_TEXT") return;
+
+    (async () => {
+        const response = await fetch("http://localhost:5000/translate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                q: msg.text,
+                source: "auto",
+                target: msg.target,
+            })
+        });
+    
+        if (!response.ok) {
+            throw new Error(`Fetch failed: ${response.status}`);
+        } 
+
+        const data = await response.json();
+        sendResponse(data);
+    })();
+    
+    return true;
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,10 @@
-let nativeLangs = ['en', 'und'];
+let nativeLangs = ["en", "und"];
 let targetNode = null;
 let observer = null;
 let intervalId = null;
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
     //if url changed, need to select new section
-    if (msg.type === 'URL_CHANGE') {
+    if (msg.type === "URL_CHANGE") {
         console.log("URL changed to: " + msg.url);
         targetNode = null;
         //select until page finished loading, in increments
@@ -32,13 +32,29 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
                         for (mutation of mutationList) {
                             if (mutation.addedNodes.length > 0) {
                                 // console.log("New mutation: ", mutation);
-                                mutation.addedNodes.forEach((node) => {
+                                mutation.addedNodes.forEach(async (node) => {
                                     //for each node, find descendent with lang attribute
                                     let langDiv = node.querySelector('div[lang]');
                                     //check if that div's lang is not in nativeLangs
                                     if (langDiv && !nativeLangs.includes(langDiv.getAttribute('lang'))) {
                                         console.log("Found foreign lang div:", langDiv); 
                                         console.log("Text content:", langDiv.textContent);
+                                        //send to background.js for translation
+                                        //CHANGE FROM HARD CODE TARGET LATER
+                                        if (langDiv.textContent.length > 0) {
+                                            try {
+                                                const response = await chrome.runtime.sendMessage({
+                                                type: "TRANSLATE_TEXT",
+                                                text: langDiv.textContent,
+                                                source: langDiv.getAttribute("lang"),
+                                                target: "en"
+                                            });
+                                            console.log("Received translation response:", response);
+                                            } catch (error) {
+                                                console.error("Error during translation request:", error);
+                                            }
+                                        }
+                                        //get the response, erase existing text and insert translated text
                                     }
                                 });
                             }
@@ -46,8 +62,8 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
                     }
                     //targetNode exists, so view existing nodes:
                     //can probably refactor this into a function so above can use as well
-                    targetNode.querySelectorAll('div[lang]').forEach(node => {
-                        if (!nativeLangs.includes(node.getAttribute('lang'))) {
+                    targetNode.querySelectorAll("div[lang]").forEach(node => {
+                        if (!nativeLangs.includes(node.getAttribute("lang"))) {
                             console.log("Found foreign lang div (initial):", node);
                             console.log("Text content:", node.textContent);
                         }
