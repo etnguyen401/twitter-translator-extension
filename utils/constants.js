c6e9feab34d5a43406f3ef8516e89819f51cc561
etnguyen401 <nguyeneric280@gmail.com>
2026-02-15 22:53:07 -0800

    add constants.js

---
+++ utils/constants.js
@@ -0,0 +1,14 @@
+// default preferences
+export const DEFAULT_PREFERENCES = {
+    TARGET_LANGUAGE: "English (en)",
+    TEXT_COLOUR: "#4606f5",
+};
+
+//storage keys
+export const STORAGE_KEYS = {
+    TARGET_LANGUAGE: "targetLanguage",
+    TEXT_COLOUR: "textColour",
+};
+
+window.DEFAULT_PREFERENCES = DEFAULT_PREFERENCES;
+window.STORAGE_KEYS = STORAGE_KEYS;
