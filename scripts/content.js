00a018b9fbeb3a7ba358c50673fb14d2bf5aa1b9
etnguyen401 <nguyeneric280@gmail.com>
2026-03-02 01:23:31 -0800

    change to switch statements for better organization

--- scripts/content.js
+++ scripts/content.js
@@ -15,95 +15,99 @@ let intervalId = null;
 
 window.addEventListener("load", () => {
     chrome.runtime.onMessage.addListener((msg, sender, response) => {
-        //if url changed, need to select new section
-        if (msg.type === MESSAGE_TYPES.URL_CHANGE) {
-            console.log("URL changed to: " + msg.url);
-            targetNode = null;
-            //select until page finished loading, in increments
-            //disconnect old observer
-            if (observer) {
-                observer.disconnect();
-                observer = null;
-            }
-            if (intervalId) {
-                clearInterval(intervalId);
-                intervalId = null;
-            }
-
-            intervalId = setInterval(() => {
-                console.log("Inside setInterval")
-                if (!targetNode) {
-                    //grab the section element
-                    targetNode = document.querySelector('section > div > div[style^="position: relative"]');
-                    if (targetNode) {
-                        //get to the right parent node that doesn't disappear on navigation
-                        targetNode = targetNode.parentElement.parentElement.parentElement;
-                        console.log("Target Node:", targetNode);
-                        //make options
-                        const options = {childList: true, subtree: true};
-                        //make callback for mutation observer
-                        const callback = function (mutationList, observer) {
-                            //for each mutation, log for now
-                            for (const mutation of mutationList) {
-                                //if mutation is an added node
-                                if (mutation.addedNodes.length > 0) {
-                                    //for each added node
-                                    mutation.addedNodes.forEach((node) => {
-                                        //if the added node has data-testid = cellInnerDiv, it's a new tweet
-                                        if (node.getAttribute && node.getAttribute("data-testid") === "cellInnerDiv"
-                                            && !node.querySelector(".translated-text")) {
-                                            //run the translated node creation
-                                            const langDiv = node.querySelector("div[lang]");
-                                            createTranslatedNode(langDiv);
-                                        }
-                                    }); 
-                                }
-                                //else if mutation is a removed node
-                                else if (mutation.removedNodes.length > 0) {
-                                    //for each removed node
-                                    mutation.removedNodes.forEach((node) => {
-                                        //when user clicks show original text 
-                                        //if the removed node matches <div class="css-175oi2r r-1awozwy r-18u37iz r-z5qs1h r-e683xz r-1s2bzr4">,
-                                        //which is the "rate this translation" div
-                                        //remake the translated node as twitter reloads the tweet
-                                        if (node.classList.contains("r-z5qs1h")) {
-                                            const langDiv = mutation.target.querySelector("div[lang]");
-                                            createTranslatedNode(langDiv);
-                                        }
-                                        //if the show more text was clicked and there already is translated text, remake the translated node with all text
-                                        else if (node.getAttribute("data-testid") === "tweet-text-show-more-link"
-                                                && mutation.target.querySelector(".translated-text")) {
-                                                const langDiv = mutation.target.querySelector("div[lang]");
-                                                mutation.target.querySelector(".translated-text").remove();
-                                                createTranslatedNode(langDiv);
-                                        }
-                                    });
-                                }
-                            }
-                        }
-                        //targetNode loaded already, so translate for existing nodes if needed:
-                        targetNode.querySelectorAll("div[lang]").forEach((node) => {
-                            if (node.querySelector(".translated-text")) return;
-                            createTranslatedNode(node);
-                        });
-                        //create mutation observer, will take over and detect new nodes
-                        observer = new MutationObserver(callback);
-
-                        //start observing
-                        observer.observe(targetNode, options);
-                        
-                        clearInterval(intervalId);
-                        intervalId = null;
-                        console.log("Interval cleared");
-                    }
-                }
-            }, 600);  
+        switch (msg.type) {
+            case MESSAGE_TYPES.URL_CHANGE:
+                console.log("URL changed to: " + msg.url);
+                initialPageSetup(targetNode, observer, intervalId);
+                break;                
         }
     });
 });
 
+function initialPageSetup(targetNode, observer, intervalId) {
+    targetNode = null;
+    //select until page finished loading, in increments
+    //disconnect old observer
+    if (observer) {
+        observer.disconnect();
+        observer = null;
+    }
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+
+    intervalId = setInterval(() => {
+        console.log("Inside setInterval")
+        if (!targetNode) {
+            //grab the section element
+            targetNode = document.querySelector('section > div > div[style^="position: relative"]');
+            if (targetNode) {
+                //get to the right parent node that doesn't disappear on navigation
+                targetNode = targetNode.parentElement.parentElement.parentElement;
+                console.log("Target Node:", targetNode);
+                //make options
+                const options = {childList: true, subtree: true};
+                //make callback for mutation observer
+                const callback = function (mutationList, observer) {
+                    //for each mutation, log for now
+                    for (const mutation of mutationList) {
+                        //if mutation is an added node
+                        if (mutation.addedNodes.length > 0) {
+                            //for each added node
+                            mutation.addedNodes.forEach((node) => {
+                                //if the added node has data-testid = cellInnerDiv, it's a new tweet
+                                if (node.getAttribute && node.getAttribute("data-testid") === "cellInnerDiv"
+                                    && !node.querySelector(".translated-text")) {
+                                    //run the translated node creation
+                                    const langDiv = node.querySelector("div[lang]");
+                                    createTranslatedNode(langDiv);
+                                }
+                            }); 
+                        }
+                        //else if mutation is a removed node
+                        else if (mutation.removedNodes.length > 0) {
+                            //for each removed node
+                            mutation.removedNodes.forEach((node) => {
+                                //when user clicks show original text 
+                                //if the removed node matches <div class="css-175oi2r r-1awozwy r-18u37iz r-z5qs1h r-e683xz r-1s2bzr4">,
+                                //which is the "rate this translation" div
+                                //remake the translated node as twitter reloads the tweet
+                                if (node.classList.contains("r-z5qs1h")) {
+                                    const langDiv = mutation.target.querySelector("div[lang]");
+                                    createTranslatedNode(langDiv);
+                                }
+                                //if the show more text was clicked and there already is translated text, remake the translated node with all text
+                                else if (node.getAttribute("data-testid") === "tweet-text-show-more-link"
+                                        && mutation.target.querySelector(".translated-text")) {
+                                        const langDiv = mutation.target.querySelector("div[lang]");
+                                        mutation.target.querySelector(".translated-text").remove();
+                                        createTranslatedNode(langDiv);
+                                }
+                            });
+                        }
+                    }
+                }
+                //targetNode loaded already, so translate for existing nodes if needed:
+                targetNode.querySelectorAll("div[lang]").forEach((node) => {
+                    if (node.querySelector(".translated-text")) return;
+                    createTranslatedNode(node);
+                });
+                //create mutation observer, will take over and detect new nodes
+                observer = new MutationObserver(callback);
+
+                //start observing
+                observer.observe(targetNode, options);
+                
+                clearInterval(intervalId);
+                intervalId = null;
+                console.log("Interval cleared");
+            }
+        }
+    }, 600);  
+}
+
 async function createTranslatedNode(langDiv) {
-    
     //check if that div's lang is not in nativeLangs
     if (langDiv && !nativeLangs.includes(langDiv.getAttribute("lang"))) {
 
